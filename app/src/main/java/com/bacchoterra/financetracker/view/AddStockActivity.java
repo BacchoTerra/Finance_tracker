0a28fdc2c7847bbdd42691873e48f4573e139714
Bruno Baccho Terra <brunobacchot@hotmail.com>
2021-01-14 17:02:53 -0300

    AddStockActivity#txtTotalPrice is now calculated..  - Need to add masks to each editText on AddStockActivity.

--- app/src/main/java/com/bacchoterra/financetracker/view/AddStockActivity.java
+++ app/src/main/java/com/bacchoterra/financetracker/view/AddStockActivity.java
@@ -3,22 +3,31 @@ package com.bacchoterra.financetracker.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bacchoterra.financetracker.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddStockActivity extends AppCompatActivity {
 
+    //Layout views
     private Toolbar toolbar;
-    private TextView txtDate,txtDataChosen;
+    private EditText editDate;
     private EditText editPrice;
     private EditText editQuantity;
     private TextView txtTotalPrice;
 
+    //Price calculation
+    float price;
+    float quantity;
 
 
     @Override
@@ -26,27 +35,108 @@ public class AddStockActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stock);
         init();
-        initToolbar();
-
+        createToolbar();
+        setEditDateToCurrentDate();
+        calculateTotal();
     }
+
+    //23renato77
 
     private void init() {
 
         toolbar = findViewById(R.id.activity_add_stock_toolbar);
-        txtDate = findViewById(R.id.activity_add_stock_txt_date);
-        txtDataChosen = findViewById(R.id.activity_add_stock_txt_choose_date);
+        editDate = findViewById(R.id.activity_add_stock_edit_date);
         editPrice = findViewById(R.id.activity_add_stock_edit_price);
         editQuantity = findViewById(R.id.activity_add_stock_edit_quantity);
-        txtTotalPrice = findViewById(R.id.activity_add_stock_txt_total_price);
+        txtTotalPrice = findViewById(R.id.activity_add_stock_txt_total_price_result);
 
     }
 
-    private void initToolbar(){
+    private void createToolbar() {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_round_arrow_back_24);
         getSupportActionBar().setTitle(null);
+
+    }
+
+    private void setEditDateToCurrentDate() {
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+
+        editDate.setText(sdf.format(calendar.getTimeInMillis()));
+
+    }
+
+    private void calculateTotal() {
+
+        editPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (!editQuantity.getText().toString().isEmpty() && charSequence.length() >0) {
+
+                    price = Float.parseFloat(charSequence.toString());
+                    quantity = Float.parseFloat(editQuantity.getText().toString());
+
+                    txtTotalPrice.setText(String.valueOf(price * quantity));
+
+                }
+
+                if (charSequence.length() == 0){
+
+                    txtTotalPrice.setText(null);
+
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        editQuantity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (!editPrice.getText().toString().isEmpty() && charSequence.length() >0) {
+
+                    quantity = Float.parseFloat(charSequence.toString());
+                    price = Float.parseFloat(editPrice.getText().toString());
+
+                    txtTotalPrice.setText(String.valueOf(price * quantity));
+
+                }
+
+                if (charSequence.length() == 0){
+
+                    txtTotalPrice.setText(null);
+
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
     }
 
@@ -57,4 +147,5 @@ public class AddStockActivity extends AppCompatActivity {
 
         return true;
     }
+
 }
